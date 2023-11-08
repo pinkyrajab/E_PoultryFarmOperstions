971eaf27c8a3e3bf118bf888bc95ed0d3bbe7392
pinkyrajab <81649511+pinkyrajab@users.noreply.github.com>
2023-11-08 21:54:44 +0300

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using IGCwebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IGCwebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+        public IActionResult productNav()
+        {
+            return View();
+        }
+        public IActionResult FarmModule()
+        {
+            return View();
+        }
+        public IActionResult BroilerFarm()
+        {
+            return View();
+        }
+        public IActionResult BreederFarm()
+        {
+            return View();
+        }
+        public IActionResult LayerFarm()
+        {
+            return View();
+        }
+        public IActionResult FeedMill()
+        {
+            return View();
+        }
+        public IActionResult Hatchery()
+        {
+            return View();
+        }
+        public IActionResult Processes()
+        {
+            return View();
+        }
+        public IActionResult SupplyChain()
+        {
+            return View();
+        }
+        public IActionResult NewsandUpdates()
+        {
+            return View();
+        }
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+        public IActionResult BookingDemo()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,31 @@
+﻿////// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+////// for details on configuring this project to bundle and minify static web assets.
+
+////// Write your JavaScript code.
+//////$(function(){
+//////    $('.selectpicker').selectpicker();
+//////});
+
+////<script type="text/javascript">
+////    $(document).ready(function () {
+////        $("#sidebar").mCustomScrollbar({
+////            theme: "minimal"
+////        });
+
+////    $('#dismiss, .overlay').on('click', function () {
+////        // hide sidebar
+////        $('#sidebar').removeClass('active');
+////    // hide overlay
+////    $('.overlay').removeClass('active');
+////        });
+
+////    $('#sidebarCollapse').on('click', function () {
+////        // open sidebar
+////        $('#sidebar').addClass('active');
+////    // fade in the overlay
+////    $('.overlay').addClass('active');
+////    $('.collapse.in').toggleClass('in');
+////    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+////        });
+////    });
+////</script>
